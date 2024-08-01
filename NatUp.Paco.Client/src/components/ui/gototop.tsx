0a889fb5fad0370e3cf512b8b3d7863738f8053a
John <john.troszynski@natup.coop>
2024-08-01 16:38:07 +0200

    fix animation + mobile position

--- NatUp.Paco.Client/src/components/ui/gototop.tsx
+++ NatUp.Paco.Client/src/components/ui/gototop.tsx
@@ -4,37 +4,44 @@ import React, { useEffect, useState } from "react";
 import { Button } from "./button";
 
 export default function GoToTop() {
-  const [visible, setVisible] = useState(false);
+  const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 10) {
-        setVisible(true);
-      } else {
-        setVisible(false);
-      }
+      setScrollY(window.scrollY);
     };
 
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  useEffect(() => {
+    console.log(scrollY);
+  }, [scrollY]);
 
   const handleClick = () => {
     //scroo to top
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
-    visible && (
+    <div
+      className="fixed bottom-[-7px] flex w-full justify-center md:justify-end md:right-36 z-50"
+      style={{
+        transform:
+          scrollY > 0
+            ? `translateY(${Math.min(0, -40 - Math.min(-40, scrollY))}px)`
+            : "translateY(40px)",
+        transition: "transform 0.3s",
+      }}
+    >
       <Button
         variant={"link"}
         onClick={handleClick}
-        style={{ animation: visible ? "fadeIn 0.5s" : "fadeOut 0.5s" }}
-        className="group"
+        className="group hover:no-underline"
       >
-        <div className="w-[215px] h-[42px] bg-go-to-top bg-cover bg-center flex items-center justify-center fixed bottom-[-10px] right-40">
+        <div className="w-[215px] h-[42px] bg-go-to-top bg-cover bg-center flex items-center justify-center">
           <div className="flex items-center justify-center mt-[-5px] gap-3">
             <div className="text-xs font-medium text-gray-600">
               Haut de page
@@ -45,6 +52,6 @@ export default function GoToTop() {
           </div>
         </div>
       </Button>
-    )
+    </div>
   );
 }
